5769b7b0cd8cbd4bc180eeb40c77a83717d332e1
Sharon Morato <sharonmorato1@gmail.com>
2018-05-01 11:07:56 -0400

    final commit

--- 02_Pokedex/pokedex.js
+++ 02_Pokedex/pokedex.js
@@ -1,32 +1,31 @@
-$( document ).ready(function() {
+$(document).ready(function() {
+// search url params
 let urlParams = new URLSearchParams(window.location.search);
 let url = urlParams.get('pokemon');
-
 // changes colors & imgs to appropriate pokemon
 $("body").addClass(url)
 $(".poke-name").text(url)
-
-console.log(`what up ${url}`)
-
-// changes symbols in stats div
+// changes symbols in stats div & evolves into for sub-img
 if (url == "charmander") {
   $("#symbol").attr("src","images/fire-symbol.png")
+  $("#sub-img-text").append(" charizard")
 }
 else if (url == "squirtle") {
   $("#symbol").attr("src","images/water-symbol.png")
+  $("#sub-img-text").append(" wartortle")
 }
 else {
   $("#symbol").attr("src","images/regular-symbol.png")
+  $("#sub-img-text").append(" persian")
 }
-// gets data for all pokemon pages from pokemon api
-
- $.get(`https://pokeapi.co/api/v2/pokemon/${url}/`, function (myResponse) {
+// Data pulled from API & displayed in stat div
+$.get(`https://pokeapi.co/api/v2/pokemon/${url}/`, function (myResponse) {
    // hp stat
-    $("#hp").text(myResponse.stats[5].base_stat)
+  $("#hp").text(myResponse.stats[5].base_stat)
    // defense stat
-    $("#defense").text(myResponse.stats[3].base_stat)
+  $("#defense").text(myResponse.stats[3].base_stat)
     // attack stat
-    $("#attack").text( myResponse.stats[4].base_stat)
+  $("#attack").text( myResponse.stats[4].base_stat)
   let i = 0;
   let txt = myResponse.abilities[0].ability.name
   let txt2 = myResponse.abilities[1].ability.name
@@ -35,7 +34,7 @@ else {
       $("#ability1").html($("#ability1").html() + txt.charAt(i));
       $("#ability2").html($("#ability2").html() + txt2.charAt(i));
       i++;
-      setTimeout(typeWriter, 80);
+      setTimeout(typeWriter, 95);
     }
   }
   typeWriter();
